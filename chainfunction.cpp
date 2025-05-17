37706cfb3861f43660deb921a1e1ef8233e7adc0
miftahuljannahhh <mj5416386@gmail.com>
2025-05-17 14:43:05 +0700

    int main

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -18,3 +18,10 @@ public:
 } bukunya;
 
 
+int main()
+{
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul();
+    return 0;
+}
