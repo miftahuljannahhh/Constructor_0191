ed7e0caff39c6e1a7a63a1bdc185f3948e4cc96f
miftahuljannahhh <mj5416386@gmail.com>
2025-05-17 15:51:03 +0700

    prosedur function

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -10,4 +10,12 @@ private:
     int id;
     string nama;
    
+    void setID();
+    void printAll();
+
+static void setNim(int pNim){nim=pNim; /*Definisi Function*/}
+static int getNim(){return nim; /*Definisi Function*/}
+
+mahasiswa(string pnama) : nama(pnama) { setID(); }
 };
+
