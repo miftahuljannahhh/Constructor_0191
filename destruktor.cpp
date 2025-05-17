fd971074b369cde67e8d2c8368418faf69fc8059
miftahuljannahhh <mj5416386@gmail.com>
2025-05-17 15:04:14 +0700

    member  functo constructor

--- destruktor.cpp
+++ destruktor.cpp
@@ -7,4 +7,17 @@ private:
     int *arr;
     int panjang;
 
+    public:
+    angka(int); // Constructor
+    ~angka();   // Destructor
+    void cetakData();
+    void isiData();
 };
+ // Definisi member Functon
+angka::angka(int i)
+{ // Constructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
